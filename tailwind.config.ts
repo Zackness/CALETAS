fcff61daec7f5cd2c71b53b84c9e54c206b44d3d
Zackness <frankensmonstersestudios@gmail.com>
2025-02-15 11:35:09 -0400

    tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,8 @@
+const { withUt } = require("uploadthing/tw")
+
 import type { Config } from "tailwindcss";
 
-export default {
+const config = withUt ({
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -9,10 +11,25 @@ export default {
   theme: {
     extend: {
       colors: {
+        'global-blue-1': '#000e16',
+  		  'global-blue-2': '#0b151c',
+  		  'global-blue-3': '#0f1d27',
+  		  'global-green': '#58CC02',
+  		  'global-green-2': '#3B841F',
+  		  'global-green-3': '#296B0F',
+  		  'global-red': '#C01515',
+  		  'global-red-2': '#AA2727',
+  		  'global-red-3': '#7E0202',
+  		  'global-yellow': '#EAD70E',
+  		  white: '#ffffff',
+  		  black: '#000000',
+  		  'monster-points': '#e8a923',
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
     },
   },
-  plugins: [],
-} satisfies Config;
+  plugins: [require("tailwindcss-animate"), require('@tailwindcss/typography'),],
+} )satisfies Config;
+
+export default config;
