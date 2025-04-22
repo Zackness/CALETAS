dee7b171759e79f9d5abc2f5e8582090d3b3ffe2
i.am.zackness <i.am.zackness@gmail.com>
2025-04-22 17:26:25 +0000

    boton de salir

--- components/header.tsx
+++ components/header.tsx
@@ -3,8 +3,16 @@
 import { SidebarTrigger } from "@/components/ui/sidebar"
 import { ThemeToggle } from "@/components/theme-toggle"
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import { logout } from "@/actions/logout";
+import { Button } from "./ui/button";
+import { ExitIcon } from "@radix-ui/react-icons";
 
 export function DashboardHeader() {
+
+  const onClick = () => {
+    logout();
+};
+  
   return (
     <header className="border-b border-border w-full h-16">
       <div className="flex h-full items-center justify-between px-4">
@@ -18,6 +26,10 @@ export function DashboardHeader() {
             <AvatarImage src="/placeholder.svg?height=32&width=32" alt="Usuario" />
             <AvatarFallback>US</AvatarFallback>
           </Avatar>
+          <Button onClick={onClick} className="w-full" type="submit" size="default" variant="outline">
+              <ExitIcon className="h-4 w-4 mr-2"/>
+                salir
+          </Button>
         </div>
       </div>
     </header>
