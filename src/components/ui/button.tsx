f3a8d90fcdc747ee7e74872bf3790f6f7c517f7d
Zackness <frankensmonstersestudios@gmail.com>
2025-05-17 22:40:22 -0400

    universidades listo, arreglar el espacio entre las partes

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -10,21 +10,21 @@ const buttonVariants = cva(
     variants: {
       variant: {
         default:
-          "bg-none border-3 items-center hover:border-white/50 cursor-pointer py-6",
+          "bg-none border-3 border-[#BEBEBE] items-center hover:border-white/50 cursor-pointer py-6",
         destructive:
           "bg-destructive text-white shadow-xs hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60",
         outline:
           "border bg-background shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50",
         secondary:
-          "bg-none border-3 border-[#40C9A9] text-[#40C9A9] cursor-pointer py-6",
+          "bg-none border-3 border-[#40C9A9] hover:border-[#40C9A9]/50 text-[#40C9A9] cursor-pointer py-6",
         ghost:
           "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50",
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
         default: "h-9 px-4 py-2 has-[>svg]:px-3",
-        sm: "h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5",
-        lg: "h-10 rounded-xl px-6 has-[>svg]:px-4 text-sm",
+        sm: "h-[63px] w-[284px] text-lg rounded-2xl gap-1.5 px-3 has-[>svg]:px-2.5",
+        lg: "h-[63px] w-[369px] rounded-2xl px-6 has-[>svg]:px-4 text-base",
         xl: "h-14 rounded-xl px-10 has-[>svg]:px-6 text-lg",
         icon: "size-9",
       },
